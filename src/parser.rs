10b321005cbf8192ba8b3a1e1d9b88b6ad60f2d9
kongo2002 <kongo2002@googlemail.com>
2023-10-03 13:28:06 +0200

    basic message parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,18 @@
+use nom::branch::alt;
 use nom::bytes::complete::is_not;
 use nom::bytes::complete::tag;
+use nom::bytes::complete::take_while1;
 use nom::character::complete::alphanumeric1;
 use nom::character::complete::char;
+use nom::character::complete::digit1;
 use nom::character::complete::multispace0;
+use nom::character::complete::multispace1;
 use nom::character::complete::space0;
 use nom::character::complete::space1;
-use nom::multi::many0;
+use nom::combinator::map_res;
+use nom::combinator::opt;
+use nom::combinator::recognize;
+use nom::multi::separated_list0;
 use nom::sequence::delimited;
 use nom::IResult;
 
@@ -15,12 +22,95 @@ use crate::errors::PtError;
 #[derive(Debug)]
 pub struct Proto {
     pub syntax: String,
-    pub messages: Vec<Message>,
+    pub elems: Vec<Elem>,
 }
 
 #[derive(Debug)]
-pub struct Message {
+pub enum Flag {
+    None,
+    Optional,
+    Repeated,
+}
+
+#[derive(Debug)]
+pub struct Field {
     pub name: String,
+    pub field_type: String,
+    pub idx: u32,
+    pub flag: Flag,
+}
+
+#[derive(Debug)]
+pub struct Rpc {
+    pub name: String,
+    pub request: String,
+    pub response: String,
+    pub stream: bool,
+}
+
+#[derive(Debug)]
+pub enum Elem {
+    Message { name: String, fields: Vec<Field> },
+    Option { name: String, value: String },
+    Import { name: String },
+    Package { name: String },
+    SingleLineComment,
+    Service { name: String, endpoints: Vec<Rpc> },
+}
+
+fn import(input: &str) -> IResult<&str, Elem> {
+    let (input, _) = tag("import")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, import) = str(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag(";")(input)?;
+
+    Ok((
+        input,
+        Elem::Import {
+            name: import.to_string(),
+        },
+    ))
+}
+
+fn package(input: &str) -> IResult<&str, Elem> {
+    let (input, _) = tag("package")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, package) = is_not(";")(input)?;
+    let (input, _) = tag(";")(input)?;
+
+    Ok((
+        input,
+        Elem::Package {
+            name: package.to_string(),
+        },
+    ))
+}
+
+fn single_line_comment(input: &str) -> IResult<&str, Elem> {
+    let (input, _) = tag("//")(input)?;
+    let (input, _) = take_while1(|chr| chr != '\r' && chr != '\n')(input)?;
+
+    Ok((input, Elem::SingleLineComment))
+}
+
+fn option(input: &str) -> IResult<&str, Elem> {
+    let (input, _) = tag("option")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, option_name) = not_space(input)?;
+    let (input, _) = space1(input)?;
+    let (input, _) = tag("=")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, value) = is_not(";")(input)?;
+    let (input, _) = tag(";")(input)?;
+
+    Ok((
+        input,
+        Elem::Option {
+            name: option_name.to_string(),
+            value: value.to_string(),
+        },
+    ))
 }
 
 fn syntax(input: &str) -> IResult<&str, &str> {
@@ -35,19 +125,122 @@ fn syntax(input: &str) -> IResult<&str, &str> {
     Ok((input, version))
 }
 
-fn message(input: &str) -> IResult<&str, Message> {
+fn field_flag(input: &str) -> IResult<&str, Flag> {
+    let (input, flag0) = opt(alt((tag("optional"), tag("repeated"))))(input)?;
+    let (input, _) = space0(input)?;
+    let flag = match flag0 {
+        Some("optional") => Flag::Optional,
+        Some("repeated") => Flag::Repeated,
+        _ => Flag::None,
+    };
+
+    Ok((input, flag))
+}
+
+fn message_field(input: &str) -> IResult<&str, Field> {
+    let (input, flag) = field_flag(input)?;
+    let (input, field_type) = not_space(input)?;
+    let (input, _) = space1(input)?;
+    let (input, name) = not_space(input)?;
+    let (input, _) = space1(input)?;
+    let (input, _) = tag("=")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, idx) = number(input)?;
+    let (input, _) = tag(";")(input)?;
+
+    Ok((
+        input,
+        Field {
+            field_type: field_type.to_string(),
+            name: name.to_string(),
+            idx,
+            flag,
+        },
+    ))
+}
+
+fn rpc(input: &str) -> IResult<&str, Rpc> {
+    let (input, _) = tag("rpc")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, name) = alphanumeric1(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag("(")(input)?;
+    let (input, _) = space0(input)?;
+    let (input, request) = alphanumeric1(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag(")")(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag("returns")(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag("(")(input)?;
+    let (input, _) = space0(input)?;
+    let (input, stream) = opt(tag("stream"))(input)?;
+    let (input, _) = space0(input)?;
+    let (input, response) = alphanumeric1(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag(")")(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag("{")(input)?;
+    let (input, _) = space0(input)?;
+    let (input, _) = tag("}")(input)?;
+
+    Ok((
+        input,
+        Rpc {
+            name: name.to_string(),
+            request: request.to_string(),
+            response: response.to_string(),
+            stream: stream.is_some(),
+        },
+    ))
+}
+
+fn service(input: &str) -> IResult<&str, Elem> {
+    let (input, _) = tag("service")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, name) = alphanumeric1(input)?;
+    let (input, _) = space1(input)?;
+    let (input, _) = tag("{")(input)?;
+    let (input, _) = multispace0(input)?;
+    let (input, rpcs) = separated_list0(multispace1, rpc)(input)?;
+    let (input, _) = multispace0(input)?;
+    let (input, _) = tag("}")(input)?;
+
+    Ok((
+        input,
+        Elem::Service {
+            name: name.to_string(),
+            endpoints: rpcs,
+        },
+    ))
+}
+
+fn message(input: &str) -> IResult<&str, Elem> {
     let (input, _) = tag("message")(input)?;
     let (input, _) = space1(input)?;
     let (input, name) = alphanumeric1(input)?;
     let (input, _) = space1(input)?;
     let (input, _) = tag("{")(input)?;
+    let (input, _) = multispace0(input)?;
+    let (input, fields) = separated_list0(multispace1, message_field)(input)?;
+    let (input, _) = multispace0(input)?;
+    let (input, _) = tag("}")(input)?;
 
     Ok((
         input,
-        Message {
+        Elem::Message {
             name: name.to_string(),
+            fields,
         },
     ))
+}
+
+fn number(input: &str) -> IResult<&str, u32> {
+    map_res(recognize(digit1), str::parse)(input)
+}
+
+fn not_space(input: &str) -> IResult<&str, &str> {
+    is_not(" \r\n\t")(input)
 }
 
 fn str(input: &str) -> IResult<&str, &str> {
@@ -57,13 +250,23 @@ fn str(input: &str) -> IResult<&str, &str> {
 fn parse0(input: &str) -> IResult<&str, Proto> {
     let (input, syntax) = syntax(input)?;
     let (input, _) = multispace0(input)?;
-    let (input, msgs) = many0(message)(input)?;
+    let (input, elems) = separated_list0(
+        multispace1,
+        alt((
+            import,
+            option,
+            message,
+            package,
+            service,
+            single_line_comment,
+        )),
+    )(input)?;
 
     Ok((
         input,
         Proto {
             syntax: syntax.to_string(),
-            messages: msgs,
+            elems,
         },
     ))
 }
